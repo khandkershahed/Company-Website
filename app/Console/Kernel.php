e23accfd5af09f7acfdf4c8adb680fce4a1efbf8
shahed141123 <khandkershahed23@gmail.com>
2024-12-23 16:24:03 +0600

    08-12-24

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->job(new SyncZKAttendance())->dailyAt('11:20'); // Run daily at 11 PM
-        $schedule->job(new SyncZKAttendance())->everyMinute();
+        $schedule->job(new SyncZKAttendance())->dailyAt('11:00');
 
     }
 
