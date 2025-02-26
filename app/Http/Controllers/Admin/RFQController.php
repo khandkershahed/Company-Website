1e2026dc1c50ba62c3623bf18c1853679f3a9d81
shahed141123 <khandkershahed23@gmail.com>
2025-02-26 10:41:16 +0600

    26-01-25

--- app/Http/Controllers/Admin/RFQController.php
+++ app/Http/Controllers/Admin/RFQController.php
@@ -216,8 +216,8 @@ class RFQController extends Controller
 
         if ($validator->fails()) {
             foreach ($validator->messages()->all() as $message) {
-                Session::flash('error', $message);
                 Toastr::error($message, 'Failed', ['timeOut' => 30000]);
+                Session::flash('error', $message);
             }
             return redirect()->back()->withInput();
         }
@@ -380,6 +380,7 @@ class RFQController extends Controller
         if ($validator->fails()) {
             foreach ($validator->messages()->all() as $message) {
                 Toastr::error($message, 'Failed', ['timeOut' => 30000]);
+                Session::flash('error', $message);
             }
             return redirect()->back()->withInput();
         }
