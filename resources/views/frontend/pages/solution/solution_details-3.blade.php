c7a5e1aa3581cb915e6e8cb19a5895cab501bda5
shahed141123 <khandkershahed23@gmail.com>
2025-03-02 13:58:12 +0600

    26-01-25

--- resources/views/frontend/pages/solution/solution_details-3.blade.php
+++ resources/views/frontend/pages/solution/solution_details-3.blade.php
@@ -139,67 +139,75 @@
                 </div>
             </div>
         </section>
-        <section class="section-padding pb-0"
-            style="margin-top: 300px; background-image: url({{ asset('images/service-bg3.jpg') }})">
-            <div class="container st-three-services-section">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card p-5 border-0 shadow-sm">
-                            <div class="card-body">
-                                <div class="text-center py-5 pt-0">
-                                    <p class="text-primary uppercase">{{ $solution->row_three_title }}</p>
-                                    <h3 class="fw-bold">{{ $solution->row_three_header }}</h3>
-                                </div>
-                                <div>
-                                    <div class="row">
-                                        <div class="col-lg-3">
-                                            <div class="p-3">
-                                                <div class="pb-4">
-                                                    <img src="{{ asset('images/ser-icon-3-1.png') }}" alt="">
-                                                </div>
-                                                <div>
-                                                    <h5 class="fw-bold">AI base business solution</h5>
-                                                    <p>Accelerate innovation with world-class tech teams We’ll match you to
-                                                        an
-                                                        entire remote team .</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-3">
-                                            <div class="p-3">
-                                                <div class="pb-4">
-                                                    <img src="{{ asset('images/ser-icon-3-2.png') }}" alt="">
-                                                </div>
-                                                <div>
-                                                    <h5 class="fw-bold">Chatbot with Real meeting</h5>
-                                                    <p>Accelerate innovation with world-class tech teams We’ll match you to
-                                                        an
-                                                        entire remote team .</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-3">
-                                            <div class="p-3">
-                                                <div class="pb-4">
-                                                    <img src="{{ asset('images/ser-icon-3-3.png') }}" alt="">
-                                                </div>
-                                                <div>
-                                                    <h5 class="fw-bold">Automate data saving</h5>
-                                                    <p>Accelerate innovation with world-class tech teams We’ll match you to
-                                                        an
-                                                        entire remote team .</p>
+        @if (!empty($solution->row_three_title) ||
+            !empty($solution->row_three_header) ||
+            !empty($solution->row_two_column_one_image) ||
+            !empty($solution->row_two_column_one_title) ||
+            !empty($solution->row_two_column_one_description) ||
+            !empty($solution->row_two_column_two_image) ||
+            !empty($solution->row_two_column_two_title) ||
+            !empty($solution->row_two_column_two_description) ||
+            !empty($solution->row_two_column_three_image) ||
+            !empty($solution->row_two_column_three_title) ||
+            !empty($solution->row_two_column_three_description) ||
+            !empty($solution->row_two_column_four_image) ||
+            !empty($solution->row_two_column_four_title) ||
+            !empty($solution->row_two_column_four_description))
+            <section class="section-padding pb-0"
+                style="margin-top: 300px; background-image: url({{ asset('images/service-bg3.jpg') }})">
+                <div class="container st-three-services-section">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card p-5 border-0 shadow-sm">
+                                <div class="card-body">
+                                    <div class="text-center py-5 pt-0">
+                                        <p class="text-primary uppercase">{{ $solution->row_three_title }}</p>
+                                        <h3 class="fw-bold">{{ $solution->row_three_header }}</h3>
+                                    </div>
+                                    <div>
+                                        <div class="row">
+                                            <div class="col-lg-3">
+                                                <div class="p-3">
+                                                    <div class="pb-4">
+                                                        <img src="{{ !empty($solution->row_two_column_one_image) && file_exists(public_path('storage/' . $solution->row_two_column_one_image)) ? asset('storage/' . $solution->row_two_column_one_image) : asset('images/ser-icon-3-1.png') }}" alt="">
+                                                    </div>
+                                                    <div>
+                                                        <h5 class="fw-bold">{{$solution->row_two_column_one_title}}</h5>
+                                                        <p>{{$solution->row_two_column_one_description}}</p>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-lg-3">
-                                            <div class="p-3">
-                                                <div class="pb-4">
-                                                    <img src="{{ asset('images/ser-icon-3-4.png') }}" alt="">
+                                            <div class="col-lg-3">
+                                                <div class="p-3">
+                                                    <div class="pb-4">
+                                                        <img src="{{ !empty($solution->row_two_column_two_image) && file_exists(public_path('storage/' . $solution->row_two_column_two_image)) ? asset('storage/' . $solution->row_two_column_two_image) : asset('images/ser-icon-3-2.png') }}" alt="">
+                                                    </div>
+                                                    <div>
+                                                        <h5 class="fw-bold">{{ $solution->row_two_column_two_title }}</h5>
+                                                        <p>{{ $solution->row_two_column_two_description }}</p>
+                                                    </div>
                                                 </div>
-                                                <div>
-                                                    <h5 class="fw-bold">Robotic Automation</h5>
-                                                    <p>Accelerate innovation with world-class tech teams We’ll match you to
-                                                        an entire remote team .</p>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="p-3">
+                                                    <div class="pb-4">
+                                                        <img src="{{ !empty($solution->row_two_column_three_image) && file_exists(public_path('storage/' . $solution->row_two_column_three_image)) ? asset('storage/' . $solution->row_two_column_three_image) : asset('images/ser-icon-3-3.png') }}" alt="">
+                                                    </div>
+                                                    <div>
+                                                        <h5 class="fw-bold">{{ $solution->row_two_column_three_title }}</h5>
+                                                        <p>{{ $solution->row_two_column_three_description }}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="p-3">
+                                                    <div class="pb-4">
+                                                        <img src="{{ !empty($solution->row_two_column_four_image) && file_exists(public_path('storage/' . $solution->row_two_column_four_image)) ? asset('storage/' . $solution->row_two_column_four_image) : asset('images/ser-icon-3-4.png') }}" alt="">
+                                                    </div>
+                                                    <div>
+                                                        <h5 class="fw-bold">{{ $solution->row_two_column_four_title }}</h5>
+                                                        <p>{{ $solution->row_two_column_four_description }}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -209,51 +217,60 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        <section>
-            <div class="st-three-counter-section-03"
-                style="background-image: url({{ asset('images/counter-bg-3.jpg') }});">
-                <div class="container">
-                    <div class="counter-wrap">
-                        <div class="row">
-                            <div class="col-lg-3 col-sm-6">
-                                <!-- Single Counter Start -->
-                                <div class="st-three-single-counter-02 text-center">
-                                    <span class="counter">354</span>
-                                    <p>Completed Projects</p>
+            </section>
+        @endif
+        @if (!empty($solution->count_one_number) ||
+            !empty($solution->count_one_text) ||
+            !empty($solution->count_two_number) ||
+            !empty($solution->count_two_text) ||
+            !empty($solution->count_three_number) ||
+            !empty($solution->count_three_text) ||
+            !empty($solution->count_four_number) ||
+            !empty($solution->count_four_text))
+            <section>
+                <div class="st-three-counter-section-03"
+                    style="background-image: url({{ asset('images/counter-bg-3.jpg') }});">
+                    <div class="container">
+                        <div class="counter-wrap">
+                            <div class="row">
+                                <div class="col-lg-3 col-sm-6">
+                                    <!-- Single Counter Start -->
+                                    <div class="st-three-single-counter-02 text-center">
+                                        <span class="counter">{{ $solution->count_one_number }}</span>
+                                        <p>{{ $solution->count_one_text }}</p>
+                                    </div>
+                                    <!-- Single Counter End -->
                                 </div>
-                                <!-- Single Counter End -->
-                            </div>
-                            <div class="col-lg-3 col-sm-6">
-                                <!-- Single Counter Start -->
-                                <div class="st-three-single-counter-02 text-center">
-                                    <span class="counter">119</span>
-                                    <p>Robotic Automation</p>
+                                <div class="col-lg-3 col-sm-6">
+                                    <!-- Single Counter Start -->
+                                    <div class="st-three-single-counter-02 text-center">
+                                        <span class="counter">{{ $solution->count_two_number }}</span>
+                                        <p>{{ $solution->count_two_text }}</p>
+                                    </div>
+                                    <!-- Single Counter End -->
                                 </div>
-                                <!-- Single Counter End -->
-                            </div>
-                            <div class="col-lg-3 col-sm-6">
-                                <!-- Single Counter Start -->
-                                <div class="st-three-single-counter-02 text-center">
-                                    <span class="counter">99</span>
-                                    <p>Web Site Analyse</p>
+                                <div class="col-lg-3 col-sm-6">
+                                    <!-- Single Counter Start -->
+                                    <div class="st-three-single-counter-02 text-center">
+                                        <span class="counter">{{ $solution->count_three_number }}</span>
+                                        <p>{{ $solution->count_three_text }}</p>
+                                    </div>
+                                    <!-- Single Counter End -->
                                 </div>
-                                <!-- Single Counter End -->
-                            </div>
-                            <div class="col-lg-3 col-sm-6">
-                                <!-- Single Counter Start -->
-                                <div class="st-three-single-counter-02 text-center">
-                                    <span class="counter">321</span>
-                                    <p>Clients Supoort Done</p>
+                                <div class="col-lg-3 col-sm-6">
+                                    <!-- Single Counter Start -->
+                                    <div class="st-three-single-counter-02 text-center">
+                                        <span class="counter">{{ $solution->count_four_number }}</span>
+                                        <p>{{ $solution->count_four_text }}</p>
+                                    </div>
+                                    <!-- Single Counter End -->
                                 </div>
-                                <!-- Single Counter End -->
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        @endif
         {{-- Solution Tree Hero Section End --}}
     </div>
     <!--=====// Pageform section //=====-->
