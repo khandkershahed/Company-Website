63086d9b348fbf1c1a3ff7d4a66869cd7ea8c9df
shahed141123 <khandkershahed23@gmail.com>
2025-04-13 15:45:28 +0600

    05-04-25

--- resources/views/frontend/pages/solution/allsolution.blade.php
+++ resources/views/frontend/pages/solution/allsolution.blade.php
@@ -108,8 +108,8 @@
                                             src="{{ isset($item->thumbnail_image) && file_exists(public_path('storage/' . $item->thumbnail_image)) ? asset('storage/' . $item->thumbnail_image) : asset('frontend/images/no-row-img(580-326).png') }}"
                                             alt="{{ $item->name }}">
                                     </div>
-                                    <div class="d-flex align-items-center" style="height: 4.4rem;">
-                                        <h5 class="p-1 mb-0" style="text-align:center;font-size: 1.1rem">{{ Str::words($item->name, 6, '...') }}
+                                    <div class="d-flex align-items-center" style="height: 5rem;">
+                                        <h5 class="p-1 mb-0" style="text-align:center;font-size: 1rem">{{ Str::words($item->name, 6, '...') }}
                                         </h5>
                                         {{-- <hr class="p-1 pt-1 m-0 text-white"> --}}
                                     </div>
