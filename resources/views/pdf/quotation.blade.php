ded2f336a930c538561fafd714d98705a6a259c0
Szamansaju <szamansaju@gmail.com>
2025-08-14 16:40:44 +0600

    Some changes done

--- resources/views/pdf/quotation.blade.php
+++ resources/views/pdf/quotation.blade.php
@@ -172,7 +172,7 @@
 
 
         <!-- Authorized Brands -->
-        <table role="presentation" width="100%" cellspacing="0" cellpadding="5"
+        <!-- <table role="presentation" width="100%" cellspacing="0" cellpadding="5"
           style="border-collapse: collapse; margin-top: 10px;">
           <tbody>
             <tr>
@@ -189,7 +189,7 @@
               @endforeach
             </tr>
           </tbody>
-        </table>
+        </table> -->
 
       </td>
     </tr>
