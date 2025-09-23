a535527d736368e9719c70bcf14d981960926e85
shahed141123 <khandkershahed23@gmail.com>
2025-09-23 15:39:48 +0600

    test email

--- resources/views/frontend/pages/rfq/rfq.blade.php
+++ resources/views/frontend/pages/rfq/rfq.blade.php
@@ -91,7 +91,7 @@
                                                                         </div>
                                                                     </div>
                                                                     <div class="mb-3">
-                                                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                                                        <button type="submit" class="btn btn-primary next-step next-btn">Submit</button>
                                                                     </div>
                                                                 </form>
 
