d6ab1670319b3f05ae8c237d3e18521e0ec600d4
shahed141123 <khandkershahed23@gmail.com>
2025-09-29 17:23:29 +0600

    test email

--- resources/views/metronic/pages/cog/crm.blade.php
+++ resources/views/metronic/pages/cog/crm.blade.php
@@ -264,7 +264,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-8 offset-lg-2">
+                    <div class="col-lg-9">
                         <div class="card shadow-none border">
                             <div class="card-header py-0 bg-light min-h-45px">
                                 <h5 class="card-title fw-semibold m-0">
