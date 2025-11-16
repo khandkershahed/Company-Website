e90d4fea22553206f46a6c77817fd6401e7cd4a2
Szamansaju <szamansaju@gmail.com>
2025-11-16 10:45:40 +0600

    Some changes dine

--- resources/views/frontend/pages/blogs/style_partial.blade.php
+++ resources/views/frontend/pages/blogs/style_partial.blade.php
@@ -1,9 +1,44 @@
 <style>
     @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
     body {
         font-family: "Poppins", sans-serif;
     }
 
+    .nav-tabs .nav-item .nav-link.active {
+        border: 1px solid transparent;
+        padding: 0;
+        border-radius: 60px;
+        font-size: 11px;
+        font-weight: 400;
+        color: var(--white);
+        background: transparent;
+    }
+
+    .page-item:not(:first-child) .page-link {
+        margin-left: -1px;
+        height: 47px;
+    }
+
+    .nav-tabs .nav-item .nav-link {
+        border: 1px solid transparent;
+        padding: 0;
+        border-radius: 60px;
+        font-size: 11px;
+        font-weight: 400;
+        color: var(--white);
+        background: transparent;
+    }
+
+    .nav-tabs .nav-item .nav-link:hover {
+        border: 1px solid transparent;
+        padding: 0;
+        border-radius: 60px;
+        font-size: 11px;
+        font-weight: 400;
+        color: var(--white);
+        background: transparent;
+    }
 
     /* Blogs Start */
     .blogs-banners {
@@ -535,5 +570,4 @@
     }
 
     /* Blogs End*/
-
 </style>
