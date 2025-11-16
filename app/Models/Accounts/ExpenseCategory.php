33a6ee14cbcc3024717f6a69c0a8522129711e9b
shahed141123 <khandkershahed23@gmail.com>
2025-11-16 12:24:11 +0600

    Catalogues Updated

---
+++ app/Models/Accounts/ExpenseCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use App\Traits\HasSlug;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseCategory extends Model
+{
+    use HasFactory,HasSlug;
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected sour
+    protected $guarded = [];
+}
---
+++ app/Models/Accounts/ExpenseType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseType extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
